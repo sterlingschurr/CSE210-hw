a2d22ca7ccb2cfddaefdb27e8c7620ea484629ca
Sterling <sterlingschurr@gmail.com>
2024-01-13 22:57:20 -0700

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent?: ");
+        int gradePercent = int.Parse(Console.ReadLine());
+        string gradeLetter;
+        if (gradePercent >= 90)
+            gradeLetter = "A";
+        else if (gradePercent >= 80)
+            gradeLetter = "B";
+        else if (gradePercent >= 70)
+            gradeLetter = "C";
+        else if (gradePercent >= 60)
+            gradeLetter = "D";
+        else
+            gradeLetter = "F";
+
+        if (gradePercent >= 70)
+            Console.WriteLine($"You passed! Your {gradePercent} got you a(n) {gradeLetter}!");
+        else
+            Console.WriteLine($"You failed. Your {gradePercent} got you a(n) {gradeLetter}, which wasn't enough. You failed, sorry pal.");
     }
 }
